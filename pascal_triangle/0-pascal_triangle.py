62a8a4cfea3634993ee178e3311502b34c64ce11
obure1always <j.obure1@alustudent.com>
2024-10-11 22:21:23 +0000

    pascal triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    pascal triangle.py
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns right angled triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
